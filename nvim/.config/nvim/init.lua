a37ad40b6bdd6de1a80d667d2dffb893fefae16d
piotrek <piotr.ruszk@gmail.com>
2022-04-15 12:03:41 +0200

    + fugitive mappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -22,6 +22,16 @@ local keymap = vim.api.nvim_set_keymap
 keymap('n', "<leader>pt", ":NERDTreeToggle<CR>", {})
 keymap('n', "<leader>pf", ":NERDTreeFind ", {})
 
+keymap('n', "<leader>fa", ":Git add .<CR>", {})
+keymap('n', "<leader>fs", ":Git status<CR>", {})
+keymap('n', "<leader>fd", ":Gvdiff :0<CR>", {})
+keymap('n', "<leader>fj", ":diffget //2<CR>", {})
+keymap('n', "<leader>fk", ":diffget //3<CR>", {})
+keymap('n', "<leader>fm", ":Gdiffsplit!<CR>", {})
+keymap('n', "<leader>fc", ":Git commit<CR>", {})
+
+keymap('n', "<leader>oo", ":only<CR>", {})
+
 
 -- This cmd will switch between absolute and relative line numbers
 vim.cmd [[
