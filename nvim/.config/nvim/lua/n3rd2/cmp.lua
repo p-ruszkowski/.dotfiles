371cb2686b4273f38aeed6a1c08dcbae5ed075cc
piotrek <piotr.ruszk@gmail.com>
2022-04-16 10:08:40 +0200

    Clear unused mappings

--- nvim/.config/nvim/lua/n3rd2/cmp.lua
+++ nvim/.config/nvim/lua/n3rd2/cmp.lua
@@ -28,10 +28,6 @@
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif vim.fn["vsnip#available"](1) == 1 then
-                feedkey("<Plug>(vsnip-expand-or-jump)", "")
-            elseif has_words_before() then
-                cmp.complete()
             else
                 fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
             end
@@ -41,8 +37,6 @@
         ["<S-Tab>"] = cmp.mapping(function()
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                feedkey("<Plug>(vsnip-jump-prev)", "")
             end
         end, { "i", "s" }),
     
